168467932f6c25b9e3d62057f76671c5d8a0fa90
Euan Thoms <euan@potensol.com>
2016-01-28 01:24:29 +0800

    Updated to version 1.2. Changed packaging from distutils to setuptools. Changed README from Markdown to ReStructured Text. Preparation for PyPi.

--- setup.py
+++ setup.py
@@ -1,22 +1,45 @@
-#!/usr/bin/env python
+import os
+from setuptools import setup
 
-from distutils.core import setup
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='netdrive-connector',
-    version='1.1',
+    version='1.2',
+    include_package_data=True,
+    license='BSD License',
     description='GUI tool to setup mountable SFTP and WebDAV connections.',
+    long_description=README,
+    url='http://github.com/ethoms/netdrive-connector/',
     author='Euan Thoms',
     author_email='euan@potensol.com',
-    url='http://www.potensol.com/projects/netdrive-connector',
-    license='BSD License',
-    platforms=['linux-*'],
+    classifiers=[
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords='connect remote network filesystem mount davfs webdav fuse sshfs sftp',
     packages=['netdriveconnector'],
-    package_dir={'netdriveconnector': 'netdriveconnector'},
+    #package_dir={'netdriveconnector': 'netdriveconnector'},
     package_data={'netdriveconnector': ['*.ui']},
     data_files=[
         ('share/pixmaps',['data/netdrive-connector.png']),
         ('share/applications',['data/netdrive-connector.desktop']),
     ],
-    scripts=['bin/netdrive-connector', 'bin/netdrive-connector_run-as-root', 'bin/add-sftp-connector', 'bin/add-webdav-connector', 'bin/remove-sftp-connector', 'bin/remove-webdav-connector'],
+    scripts=['bin/netdrive-connector_run-as-root', 'bin/add-sftp-connector', 'bin/add-webdav-connector', 'bin/remove-sftp-connector', 'bin/remove-webdav-connector'],
+    entry_points={
+        'console_scripts': [ 'netdrive-connector = netdriveconnector.main:main',],
+    },
 )
